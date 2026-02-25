fe3a93a1f9636e5deb16e4b7445645a808729fd8
Ekaterina Sivkova <Katasivkova352@gmail.com>
2026-02-25 15:46:30 +0500

    Добавьте файлы проекта.

---
+++ Task1/Lab1_Task1/Lab1_Task1.cpp
@@ -0,0 +1,217 @@
+﻿//Археолог нашел N артефактов.Известны веса(сi) и объемы(di) артефактов.Нужно выбрать такое подмножество
+//найденных вещей, чтобы суммарный их вес не превысил B кг, но был наиболее близок к B, а количество 
+//взятых артефактов было как можно больше.Известно, что решение единственно.Укажите порядковые номера 
+//вещей, которые надо взять.Исходные данные находятся в текстовом файле, в первой строке указаны N и B, 
+//а во второй строке значения весов(в кг), в третьей объемы находок(в куб.см).Вывести так же суммарный 
+//вес и суммарный объем результата.
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+// FIX_ME: using namespace std в глобальной области видимости
+//using namespace std;
+
+// FIX_ME: имя функции должно начинаться с заглавной буквы и не должно содержать нижних подчёркиваний
+// FIX_ME: параметры должны начинаться с заглавной буквы и быть информативными
+// FIX_ME: лишние пробелы в угловых скобках
+//void find_numbers(vector <vector <int>> &Mvector, int i, int j, vector <int> &Wvector)
+void FindNumbers(std::vector<std::vector<int>>& Matrix, int Row, int Col, std::vector<int>& Weights)
+{
+    //FIX_ME: отсутствуют фигурные скобки для if
+    //FIX_ME: параметры обновлены
+    //if (Mvector[i][j] == 0)
+    if (Matrix[Row][Col] == 0)
+    {
+        return;
+    }
+
+    // FIX_ME: отсутствуют пробелы вокруг бинарного оператора '-'
+    // FIX_ME: отсутствуют фигурные скобки для if
+    // FIX_ME: параметры обновлены
+    //if (Mvector[i-1][j] == Mvector[i][j])
+    if (Matrix[Row - 1][Col] == Matrix[Row][Col])
+    {
+        // FIX_ME: имя функции изменено, параметры обновлены
+        //find_numbers(Mvector, i - 1, j, Wvector);
+        FindNumbers(Matrix, Row - 1, Col, Weights);
+    }
+    else
+    {
+        // FIX_ME: добавлен std::cout
+        // FIX_ME: обновлён параметр
+        //cout << i << " ";
+        std::cout << Row << " ";
+        // FIX_ME: имя функции изменено, параметры обновлены
+        //find_numbers(Mvector, i - 1, j, Wvector);
+        FindNumbers(Matrix, Row - 1, Col, Weights);
+    }
+}
+int main()
+{
+    // FIX_ME: переменные должны быть инициализированы при объявлении
+    // FIX_ME: имена переменных должны начинаться с большой буквы и не должны содержать нижжних подчёркиваний
+    //int n, b, k, weight, volume, ind_i, ind_j;
+    int N = 0, B = 0, K = 0, Weight = 0, Volume = 0, IndexI = 0, IndexJ = 0;
+
+    // FIX_ME: добавить std::
+    // FIX_ME: имя переменной должно начинаться с большой буквы
+    //ifstream stream;
+    std::ifstream Stream;
+
+    // FIX_ME: обновлены имена переменных
+    //stream.open("a.txt");
+    //stream >> n;
+    //stream >> b;
+    Stream.open("a.txt");
+    Stream >> N;
+    Stream >> B;
+
+    // FIX_ME: добавить std::
+    //FIX_ME: обновлено имя переменной
+    // FIX_ME: отсутствуют пробелы вокруг бинарного оператора '+'
+    // FIX_ME: имя переменной должно начинаться с заглавной буквы и не должно содержать нижних подчёркиваний
+    //vector <int> weight_vector(n+1);
+    std::vector<int> Weights(N + 1);
+
+    // FIX_ME: отсутствуют фигурные скобки для for
+    // FIX_ME: использовать префиксный инкремент
+    // FIX_ME: обновлены имена переменных
+    //for (int i = 1; i <= n; i++)
+    //    stream >> weight_vector[i];
+    for (int i = 1; i <= N; ++i)
+    {
+        Stream >> Weights[i];
+    }
+
+    // FIX_ME: добавить std::
+    //FIX_ME: обновлено имя переменной
+    // FIX_ME: отсутствуют пробелы вокруг бинарного оператора '+'
+    // FIX_ME: имя переменной должно начинаться с заглавной буквы и не должно содержать нижних подчёркиваний
+    //vector <int> volume_vector(n+1);
+    std::vector<int> Volumes(N + 1);
+
+    // FIX_ME: отсутствуют фигурные скобки для for
+    // FIX_ME: использовать префиксный инкремент
+    // FIX_ME: обновлены имена переменных
+    //for (int i = 1; i <= n; i++)
+    //    stream >> volume_vector[i];
+    for (int i = 1; i <= N; ++i)
+    {
+        Stream >> Volumes[i];
+    }
+
+    // FIX_ME: добавить std::
+    // FIX_ME: имена переменных должны быть информативными, начинаться с заглавной буквы и не должны содержать нижних подчёркиваний
+    // FIX_ME: лишние пробелы между угловыми скобками
+    // FIX_ME: отсутствуют пробелы вокруг бинарного оператора '+'
+    //vector <vector <int>> main_vector(n+1, vector <int>(b+1, 0));
+    //vector <vector <int>> sum_volume_vector(n+1, vector <int>(b+1, 0));
+    std::vector<std::vector<int>> WeightMatrix(N + 1, std::vector<int>(B + 1, 0));
+    std::vector<std::vector<int>> VolumeMatrix(N + 1, std::vector<int>(B + 1, 0));
+
+    // FIX_ME: использовать префиксный инкремент
+    //FIX_ME: обновлено имя переменной
+    //for (int i = 1; i <= n; i++)
+    for (int i = 1; i <= N; ++i)
+    {
+        // FIX_ME: использовать префиксный инкремент
+        //FIX_ME: обновлено имя переменной
+        //for (int j = 1; j <= b; j++)
+        for (int j = 1; j <= B; ++j)
+        {
+            //FIX_ME: обновлено имя переменной
+            //if (j >= weight_vector[i])
+            if (j >= Weights[i])
+            {
+                // FIX_ME: отсутствуют пробелы вокруг бинарного оператора '-'
+                // FIX_ME: обновлены имена переменных
+                //main_vector[i][j] = main_vector[i-1][j] + weight_vector[i];
+                //sum_volume_vector[i][j] = sum_volume_vector[i-1][j] + volume_vector[i];
+                WeightMatrix[i][j] = WeightMatrix[i - 1][j] + Weights[i];
+                VolumeMatrix[i][j] = VolumeMatrix[i - 1][j] + Volumes[i];
+            }
+            else
+            {
+                // FIX_ME: отсутствуют пробелы вокруг бинарного оператора '-'
+                // FIX_ME: обновлены имена переменных
+                //main_vector[i][j] = main_vector[i-1][j];
+                //sum_volume_vector[i][j] = sum_volume_vector[i-1][j];
+                WeightMatrix[i][j] = WeightMatrix[i - 1][j];
+                VolumeMatrix[i][j] = VolumeMatrix[i - 1][j];
+            }
+        }
+    }
+    // FIX_ME: обновлены имена переменных
+    //k = b;
+    K = B;
+
+    //FIX_ME: булевы переменные должны начинаться с префикса 'b'
+    //bool flag = true;
+    bool bFlag = true;
+
+    // FIX_ME: избыточное сравнение с true
+    // FIX_ME: использовать префиксный инкремент
+    // FIX_ME: обновлены имена переменных
+    //while (flag == true && k >= 1)
+    //{
+    //    for (int i = n; i >= 1; i--)
+    //    {
+    //        for (int j = b; j >= 1; j--)
+    //        {
+    //            if (main_vector[i][j] == k)
+    //            {
+    //                ind_i = i; ind_j = j;
+    //                weight = main_vector[i][j];
+    //                volume = sum_volume_vector[i][j];
+    //                flag = false;
+    //            }
+    while (bFlag && K >= 1)
+    {
+        for (int i = N; i >= 1; --i)
+        {
+            for (int j = B; j >= 1; --j)
+            {
+                if (WeightMatrix[i][j] == K)
+                {
+
+                    IndexI = i;
+                    IndexJ = j;
+                    Weight = WeightMatrix[i][j];
+                    Volume = VolumeMatrix[i][j];
+                    bFlag = false;
+                }
+                // FIX_ME: избыточное сравнение с false
+                //FIX_ME: обновлено имя переменной
+                // FIX_ME: отсутствуют фигурные скобки для if
+                //if (flag == false)
+                if (!bFlag)
+                {
+                    break;
+                }
+            }
+            // FIX_ME: избыточное сравнение с false
+            //FIX_ME: обновлено имя переменной
+            // FIX_ME: отсутствуют фигурные скобки для if
+            //if (flag == false)
+            if (!bFlag)
+            {
+                break;
+            }
+        }
+        // FIX_ME: использовать префиксный декремент
+        //FIX_ME: обновлено имя переменной
+        //k--;
+        --K;
+    }
+    // FIX_ME: обновлены имена переменных
+    // FIX_ME: добавить std::cout
+    //cout << "weight: " << weight << " volume: " << volume << endl;
+    //cout << "artifact numbers: ";
+    std::cout << "weight: " << Weight << " volume: " << Volume << std::endl;
+    std::cout << "artifact numbers: ";
+    // FIX_ME: имя функции изменено, параметры обновлены
+    //find_numbers(main_vector, ind_i, ind_j, weight_vector);
+    FindNumbers(WeightMatrix, IndexI, IndexJ, Weights);
+}
---
+++ Task3/Lab1_Task3/Lab1_Task3.cpp
@@ -0,0 +1,80 @@
+﻿//К - ичные числа.Среди чисел в системе счисления с основанием K(2≤K≤10) определить 
+//сколько имеется чисел из N(1 < N < 20, N + K < 26) разрядов таких, что в их записи 
+//содержится два и более подряд идущих нулей.Для того, чтобы избежать переполнения, ответ 
+//представьте в виде вещественного числа.
+
+#include <iostream>
+#include <cmath>
+
+// FIX_ME: using namespace std в глобальной области видимости
+//using namespace std
+
+//FIX_ME: фигурная скобка должна быть на новой строке
+//int main() {
+int main()
+{
+    // FIX_ME: добавлена поддержка русского языка
+    setlocale(0, "");
+
+    // FIX_ME: имена переменных должны быть информативными и начинаться с заглавной буквы
+    // FIX_ME: переменные должны быть инициализированы при объявлении
+    //int k, n;
+    int K = 0, N = 0;
+
+    // FIX_ME: добавить std::
+    // FIX_ME: отсутствуют пробелы вокруг операторов сравнения
+    // FIX_ME: обновлены имена переменных
+    //cout << "Введите K (2<=K<=10) : ";
+    //cin >> k;
+    //cout << "Введите N (1<N<20, N+K<26) : ";
+    //cin >> n;
+    std::cout << "Введите K (2 <= K <= 10) : ";
+    std::cin >> K;
+
+    std::cout << "Введите N (1 < N < 20, N + K < 26) : ";
+    std::cin >> N;
+
+    // FIX_ME: имена переменных должны быть информативными и начинаться с заглавной буквы
+    // FIX_ME: переменные должны быть инициализированы при объявлении
+    // FIX ME: комментарии должны заканчиваться точкой
+    //double nz, oz, tz;
+    //nz = k - 1; // Числа без ведущих нулей
+    //oz = 1;     // Числа, заканчивающиеся на 0 (без подряд идущих нулей)
+    //tz = 0;     // Числа, содержащие подряд идущие нули
+    double NumbersWithoutZero = K - 1; // Числа без ведущих нулей.
+    double NumbersEndingWithZero = 1; // Числа, заканчивающиеся на 0 (без подряд идущих нулей).
+    double NumbersWithTwoZeros = 0; // Числа, содержащие подряд идущие нули.
+
+    // FIX_ME: фигурная скобка должна быть на новой строке
+    // FIX_ME: имена переменных должны быть информативными
+    // FIX_ME: использовать префиксный инкремент
+    // FIX_ME: обновлены имена переменных
+    //for (int i = 2; i <= n; i++) {
+    //    double _nz = nz;
+    //    double _oz = oz;
+    //    double _tz = tz;
+
+    //    nz = (_nz + _oz) * (k - 1);
+    //    oz = _nz;
+    //    tz = _tz * k + _oz;
+    //}
+    for (int i = 2; i <= N; ++i)
+    {
+        double PrevNumbersWithoutZero = NumbersWithoutZero;
+        double PrevNumbersEndingWithZero = NumbersEndingWithZero;
+        double PrevNumbersWithTwoZeros = NumbersWithTwoZeros;
+
+        NumbersWithoutZero = (PrevNumbersWithoutZero + PrevNumbersEndingWithZero) * (K - 1);
+        NumbersEndingWithZero = PrevNumbersWithoutZero;
+        NumbersWithTwoZeros = PrevNumbersWithTwoZeros * K + PrevNumbersEndingWithZero;
+    }
+
+    // FIX_ME: добавить std
+    // FIX_ME: обновлены имена переменных
+    //cout << "Числа без двух подряд идущих нулей: " << fixed << nz + oz << "\n";
+    //cout << "Числа с двумя и более подряд идущими нулями: " << fixed << tz << "\n";
+    std::cout << "Числа без двух подряд идущих нулей: " << std::fixed << NumbersWithoutZero + NumbersEndingWithZero << "\n";
+    std::cout << "Числа с двумя и более подряд идущими нулями: " << std::fixed << NumbersWithTwoZeros << "\n";
+
+    return 0;
+}
